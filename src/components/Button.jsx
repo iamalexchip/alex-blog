cbf7624e463f999bfdc61a5819245e492a873f04
Alex <iamalexchip@gmail.com>
2023-04-20 20:37:34 +0200

    Add button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 
-const Button = () => (
-  <button className="border border-green-700 p-8"></button>
+const Button = ({ title }) => (
+  <button className="border border-green-700 p-8">{title}</button>
 );
 
 export default Button;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import { MDXProvider } from '@mdx-js/react'
 
 const components = {
-  h1: (props) => <h1 {...props} className="text-xl font-light" />
+  h1: (props) => <h1 {...props} className="text-2xl font-bold" />
 }
 
 export default function App({ Component, pageProps }) {
