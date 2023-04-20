caa6d97295aa58156b580fb811ee475010d59f6c
Alex <iamalexchip@gmail.com>
2023-04-20 20:28:57 +0200

    Mdx and stuff

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+
+const Button = () => (
+  <button className="border border-green-700 p-8"></button>
+);
+
+export default Button;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {
+  h1: (props) => <h1 {...props} className="text-xl font-light" />
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
 }
